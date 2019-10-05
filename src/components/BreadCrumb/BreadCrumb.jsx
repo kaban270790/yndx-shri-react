e5823abddb58c2d190fbba158fe9bda810650b4c
d.timofeev <Dima27.07.90@mail.ru>
2019-10-05 16:23:16 +0300

    мелкие правки в хлебных крошках

--- src/components/BreadCrumb/BreadCrumb.jsx
+++ src/components/BreadCrumb/BreadCrumb.jsx
@@ -28,17 +28,18 @@ export default (props) => {
         lHeight: 20,
         size: 14,
         color: 'gray',
+        underline: 'non',
         marginR: 3
     };
     return <div className={cnBreadCrumb({borderB: true})}>
         {listCrumbs.map((crumb, index) => {
             const isLast = (index + 1) === listCrumbs.length;
             return (<>
-                <Text tag={'a'}
+                <Text tag={isLast ? 'span' : 'a'}
                       href={crumb.link}
-                      mods={{...textMods, ...(isLast ? {color: 'black', underline: 'non', width: 'bold'} : {})}}
+                      mods={{...textMods, ...(isLast ? {color: 'black', width: 'bold'} : {})}}
                 >{crumb.name}</Text>
-                {isLast ? null : <Text mods={textMods}>/</Text>}
+                {isLast ? null : <Text mods={textMods}>&nbsp;/&nbsp;</Text>}
             </>);
         })}
     </div>;
