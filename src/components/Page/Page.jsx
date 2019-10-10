aeab7aa6e903fbe1e6a34d4c246f9bf266cffe02
d.timofeev <Dima27.07.90@mail.ru>
2019-10-10 21:59:21 +0300

    Перевел компонент Footer на ts

--- src/components/Page/Page.jsx
+++ src/components/Page/Page.jsx
@@ -2,7 +2,7 @@ import './Page.scss';
 import React from "react";
 import {cn} from "@bem-react/classname";
 import Header from "../Header/Header.jsx";
-import Footer from "../Footer/Footer.jsx";
+import Footer from "../Footer/Footer";
 import Article from "../Article/Article.jsx";
 
 const cnPage = cn('Page');
