db4e4e354d85fa1f27b841a0caf870b8f0ae6160
d.timofeev <Dima27.07.90@mail.ru>
2019-10-10 21:05:13 +0300

    Удалил куски первого компонента для тестов

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,8 @@
 import React from "react";
-import {actionTick, actionSetRepositories} from "../src/lib/store.js";
+import {actionSetRepositories} from "../src/lib/store.js";
 import {connect} from "react-redux";
-import Time from '../src/components/Time/Time.jsx';
 import nextExpressPage from "next-express/page";
 import App from "../src/components/App/App.jsx";
-import TabList from "../src/components/TabList/TabList.jsx";
-import TableMobile from "../src/components/TableMobile/TableMobile.jsx";
-import Files from "../src/components/Files/Files.jsx";
-import {withRouter} from "next/router.js";
 
 const getReposList = require('../src/server/getReposList.js');
 
