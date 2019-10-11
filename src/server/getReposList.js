0a7759117971c9dee450a2164b8f6b9122144908
d.timofeev <Dima27.07.90@mail.ru>
2019-10-11 19:48:40 +0300

    Добавил в получение репозиториев получение информации по последним комитам

--- src/server/getReposList.js
+++ src/server/getReposList.js
@@ -1,11 +1,13 @@
 const fs = require("fs");
+const {resolve: pathResolve} = require("path");
+const gitLog = require("./git/Log.js");
 /**
  *
  * @param {string} reposDir
  * @returns {Promise}
  */
 module.exports = (reposDir) => {
-    return (new Promise((resolve, reject) => {
+    const promise = (new Promise((resolve, reject) => {
         fs.readdir(reposDir, {withFileTypes: true}, (err, files) => {
             if (err) {
                 reject(err)
@@ -18,7 +20,25 @@ module.exports = (reposDir) => {
         return files.map(pathStat => {
             return {
                 name: pathStat.name,
+                // lastCommit: {},
             }
         });
+    }).then(repositories => {
+        let promiseGetCommits = (new Promise(resolve => resolve(repositories)));
+        repositories.map((repository, index) => {
+            promiseGetCommits.then(async (repositories) => {
+                return await gitLog(pathResolve(reposDir, repository.name), {
+                    pageLimit: 1,
+                }).then(commits => {
+                    if (commits && commits.length > 0) {
+                        repositories[index].lastCommit = commits.shift();
+                    }
+                    return repositories
+                });
+            });
+        });
+        return promiseGetCommits;
     });
+
+    return promise;
 };
