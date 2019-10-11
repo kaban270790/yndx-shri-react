669f306bb9af52945c6bdcf2509d9ce49351cdd7
d.timofeev <Dima27.07.90@mail.ru>
2019-10-11 19:47:54 +0300

    Создал файл с методом для получения лога информации по комитам

---
+++ src/server/git/Log.js
@@ -0,0 +1,47 @@
+const {execFile} = require('child_process');
+
+/**
+ * @param {string} reposDir
+ * @param {{pageLimit?:number, page?:number, hash?:string, path?:string}}params
+ * @returns {Promise<{}>}
+ */
+module.exports = (reposDir, params) => {
+    params = params || {};
+    return (new Promise((resolve, reject) => {
+        const commitDelimiter = '-----';
+        const format = [commitDelimiter, '%H', '%at', '%s'].join('%n');
+        let options = [
+            'log',
+            `--format=${format}`,
+        ];
+        if (params.pageLimit > 0) {
+            options.push(`-${params.pageLimit}`);
+        }
+        if (params.page > 0) {
+            options.push(`--skip=${(params.page - 1) * params.pageLimit}`);
+        }
+        if (params.hash) {
+            options.push(params.hash);
+        }
+        if (params.path) {
+            options.push(params.path);
+        }
+
+        execFile('git', options, {cwd: reposDir}, (err, data, errMess) => {
+            if (err && errMess) {
+                reject(errMess);
+            }
+            let commits = data.split(commitDelimiter)
+                .map(value => value.trim())
+                .filter(value => value.length > 0)
+                .map(value => {
+                    let commitData = value.trim().split("\n");
+                    let hash = commitData.shift();
+                    let timestamp = commitData.shift();
+                    let source = commitData.join("/n");
+                    return {hash, timestamp, source};
+                });
+            resolve(commits);
+        });
+    }));
+};
