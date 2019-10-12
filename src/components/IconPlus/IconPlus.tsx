ecf15ca7d8c549eb4b4f0347f5e2be4c67692530
d.timofeev <Dima27.07.90@mail.ru>
2019-10-12 07:03:16 +0300

    Перевел компонент IconPlus на ts

--- src/components/IconPlus/IconPlus.tsx
+++ src/components/IconPlus/IconPlus.tsx
@@ -1,10 +1,19 @@
 import './IconPlus.scss';
-import React from "react";
+import React, {ReactNode} from "react";
 import {cn} from "@bem-react/classname";
 
 const cnIconPlus = cn('IconPlus');
 
-export default (props) => {
+type Mods = {
+}
+
+interface Props {
+    mods?: Mods;
+    className?: string;
+    children: ReactNode
+}
+
+export default (props: Props) => {
     return <div className={cnIconPlus(props.mods || {})}>
         {props.children}
     </div>;
